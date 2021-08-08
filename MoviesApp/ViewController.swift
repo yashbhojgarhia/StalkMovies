88129cf5c7c0346a96af6772e6351245f7366fe2
Yash Bhojgarhia <yash.bhojgarhia@jeevansathi.com>
2021-08-08 15:58:11 +0530

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Yash Bhojgarhia on 08/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
